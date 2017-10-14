fe766f70b3a2300e77822dbcc296330504b3fcd6
Richard Kotze <rkotze@findmypast.com>
2017-10-14 23:56:56 +0100

    Trying out JEST snapshot testing.

---
+++ src/gallery.spec.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import Gallery from './gallery';
+import renderer from 'react-test-renderer';
+
+describe('Render the gallery', () => {
+    let galleryComponent;
+    beforeAll(() => {
+        galleryComponent = renderer.create(
+            <Gallery 
+            photoList={
+                [
+                {
+                    photo: '/assets/main01.jpg',
+                    title: 'Abstract art ',
+                    thumbnail: '/assets/thumb01.jpg'
+                },
+                {
+                    photo: '/assets/main02.jpg',
+                    title: 'Riding the wave',
+                    thumbnail: '/assets/thumb02.jpg'
+                },
+                {
+                    photo: '/assets/main03.jpg',
+                    title: 'Sushi dinner',
+                    thumbnail: '/assets/thumb03.jpg'
+                },
+                {
+                    photo: '/assets/main04.jpg',
+                    title: 'Cat photo',
+                    thumbnail: '/assets/thumb04.jpg'
+                }
+                ]
+            } />
+        );
+    });
+
+    it('the initial state showing first image selected', () => {
+        let gallerySnap = galleryComponent.toJSON();
+        expect(gallerySnap).toMatchSnapshot();
+    });
+
+    it('go to next image', () => {
+        galleryComponent.getInstance().next();
+        let gallerySnap = galleryComponent.toJSON();
+        expect(gallerySnap).toMatchSnapshot();
+    });
+});
