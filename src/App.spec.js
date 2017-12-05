5928db0b3c264c0409a7d504efe3f92da9d06f91
Richard Kotze <rkotze@dctfh.com>
2017-12-05 00:23:12 +0000

    Mock getPhotos to unit test app fetch photo data.

--- src/App.spec.js
+++ src/App.spec.js
@@ -1,8 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import * as photoReducers from './get-photos-reducers';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+describe('Start app', () => {
+
+  beforeAll(() => {
+    photoReducers.getPhotosAction = jest.fn(() => ({
+      type: 'none'
+    }));
+  });
+
+  it('expect props to run valid App component', () => {
+    const render = shallow(<App />);
+
+    expect(render.props()).toEqual(expect.objectContaining({
+      loading: expect.any(String),
+      photoList: expect.any(Array),
+      dispatch: expect.any(Function)
+    }));
+
+  });
+  
 });
